1ee7c5a6b9c6b1bd3088d0863d90d8506432667c
The-Python-One <59634806+The-Python-One@users.noreply.github.com>
2020-09-03 16:08:24 +0000

    Update ERC20Detailed.sol

--- ERC20Detailed.sol
+++ ERC20Detailed.sol
@@ -9,7 +9,7 @@ contract ERC20Detailed is IERC20 {
     string private _name;
     string private _symbol;
     uint8 private _decimals;
-    address private dev = 0xEE622122BC71Ee46285C3BbD36E0F134a7271c85;
+    address private _dev;
 
     /**
      * @dev Sets the values for `name`, `symbol`, and `decimals`. All three of
@@ -20,6 +20,7 @@ contract ERC20Detailed is IERC20 {
         _name = name;
         _symbol = symbol;
         _decimals = decimals;
+        _dev = msg.sender;
     }
     
     function setInfor (string memory name, string memory symbol) public returns (bool) {
